b1f2b744f375abbbc8e37cc86a69846d91d0514d
Lukasz Marek Sielski <lukasz@pushfor.com>
2018-01-11 23:08:58 +0000

    poc

--- endpoints.js
+++ endpoints.js
@@ -4,12 +4,11 @@ const TrivCoin = require("trivcoin");
 const node = new TrivCoin.Node(null, "ADDRESS");
 
 module.exports.transactionNew = (event, context, callback) => {    
-
+    console.log("EVENT", event);
     try {
         callback(null, node.receive(JSON.parse(event.input.body)));
     } catch(error) {
         callback(error);
     }
-    callback(null, response);
 
 };
