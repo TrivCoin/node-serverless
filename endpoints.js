d51077e3ed0d3010c6ec8137cd05a96beb223b61
Lukasz Marek Sielski <lukasz@pushfor.com>
2018-01-12 21:33:27 +0000

    test

--- endpoints.js
+++ endpoints.js
@@ -5,11 +5,11 @@ const S3DB = require("./s3");
 const node = new TrivCoin.Node(null, "ADDRESS", new S3DB("blocks"), new S3DB("transactions"));
 
 module.exports.rpc = (event, context, callback) => {
+    console.log("ENV", process.env);
     try {
         node
             .receive(JSON.parse(event.body))
             .then(response => {
-                console.log("ENV", process.env);
                 callback(null, {
                     "isBase64Encoded": false,
                     "statusCode": response.error ? 400 : 200,
@@ -18,10 +18,32 @@ module.exports.rpc = (event, context, callback) => {
                 });
             })
             .catch(error => {
-                callback(error);
+                callback(null, {
+                    "isBase64Encoded": false,
+                    "statusCode": 500,
+                    "headers": { "Content-Type": "application/json" },
+                    "body": JSON.stringify({
+                        "error": {
+                            "code": error.code,
+                            "message": error.message,
+                            "details": error
+                        }
+                    })
+                });
             });
     } catch (error) {
-        callback(error);
+        callback(null, {
+            "isBase64Encoded": false,
+            "statusCode": 500,
+            "headers": { "Content-Type": "application/json" },
+            "body": JSON.stringify({
+                "error": {
+                    "code": error.code,
+                    "message": error.message,
+                    "details": error
+                }
+            })
+        });
     }
 
 };
