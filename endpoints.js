f63dbb5d2ac23e04cd548914f65e41f4a0656177
Lukasz Marek Sielski <lukasz@pushfor.com>
2018-01-12 00:35:25 +0000

    poc

--- endpoints.js
+++ endpoints.js
@@ -1,21 +1,25 @@
 'use strict';
 
 const TrivCoin = require("trivcoin");
-const node = new TrivCoin.Node(null, "ADDRESS");
+const S3DB = require("./s3");
+const node = new TrivCoin.Node(null, "ADDRESS", new S3DB("blocks"), new S3DB("transactions"));
 
-
-
-module.exports.rpc = (event, context, callback) => {    
+module.exports.rpc = (event, context, callback) => {
     try {
-        const response = node.receive(JSON.parse(event.body));        
-        console.log("ENV", process.env);
-        callback(null, {
-            "isBase64Encoded": false,
-            "statusCode": response.error ? 400 : 200,
-            "headers": { "Content-Type": "application/json" },
-            "body": JSON.stringify(response)
-        });
-    } catch(error) {
+        node.receive(JSON.parse(event.body))
+            .then(respone => {
+                console.log("ENV", process.env);
+                callback(null, {
+                    "isBase64Encoded": false,
+                    "statusCode": response.error ? 400 : 200,
+                    "headers": { "Content-Type": "application/json" },
+                    "body": JSON.stringify(response)
+                });
+            })
+            .catch(error => {
+                callback(error);
+            });
+    } catch (error) {
         callback(error);
     }
 
--- s3.js
+++ s3.js
@@ -1,41 +1,139 @@
 
 const S3Class = require('aws-sdk/clients/s3')
 const S3 = new S3Class();
+const BUCKET = process.env.BUCKET;
+
+/**
+ * Create object on S3
+ * @param {string} key
+ * @param {object} body
+ * @returns {Promise}
+ */
+const create = (key, body) => new Promise((resolve, reject) => S3
+    .putObject({
+        Bucket: BUCKET,
+        Key: key,
+        Body: JSON.stringify(body),
+        ContentType: 'application/json'
+    }, err => {
+        if (err) {
+            console.log(err);
+            reject(err);
+            return;
+        }
+        resolve();
+    }));
+
+/**
+ * Check if object exists on S3
+ * @param {string} key
+ * @returns {Promise<boolean>}
+ */
+const exists = key => new Promise((resolve, reject) => S3
+    .headObject({
+        Bucket: BUCKET,
+        Key: key
+    }, (err, data) => {
+        if (err) {
+            resolve(false);
+        } else {
+            resolve(true);
+        }
+    }));
+
+/**
+ * Reads content of the object on s3
+ * @param {string} key
+ * @return {Promise<object>}
+ */
+const read = (key) => new Promise((resolve, reject) => S3
+    .getObject({
+        Bucket: BUCKET,
+        Key: key
+    }, (err, data) => {
+
+        if (err) {
+            if (err.code === "NoSuchKey") {
+                resolve();
+            }
+            else {
+                reject(err);
+            }
+
+            return;
+        }
+
+        resolve(JSON.parse(data.Body.toString('utf-8')));
+    }));
+
+/**
+ * Deletes object from S3
+ * @param {key}
+ * @return {Promise}
+ */
+const remove = (key) => new Promise((resolve, reject) => S3
+    .deleteObject({
+        Bucket: BUCKET,
+        Key: key
+    }, err => {
+        if (err) {
+            resolve(false);
+        } else {
+            resolve(true);
+        }
+    }));
+    
+/**
+ * List all objects in s3 after given key
+ * @param {string} afterKey
+ * @param {number} length
+ * @return {Promise<string[]>}
+ */
+const scan = (afterKey, length, prefix) => new Promise((resolve, reject) => S3
+    .listObjectsV2({
+        Bucket: BUCKET,
+        StartAfter: afterKey,
+        MaxKeys: length,
+        Prefix: prefix || undefined
+    }, (err, data) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve(data.Contents.map(data => read(data.Key)));
+    }));
+
+    
+/**
+ * List all objects in s3 after given key
+ * @param {string} afterKey
+ * @param {number} length
+ * @return {Promise<object[]>}
+ */
+const fetch = (afterKey, length, prefix) => new Promise((resolve, reject) => 
+    scan(afterKey, length, prefix)
+    .then(keys => Promise.all(keys.map(key => read(key))));
 
 class S3DB {
 
-    constructor() {
-        this.collection = {};
+    constructor(prefix) {
+        this.prefix = prefix;
     }
 
     put(id, data) {
-        const that = this;
-        return new Promise(resolve => {
-            that.collection[id] = data;
-            resolve(data);
-        });
+        return create(this.prefix + "/" + id, data);
     }
 
     get(id) {
-        const that = this;
-        return new Promise(resolve => {
-            resolve(that.collection[id]);
-        });
+        return read(this.prefix + "/" + id);
     }
 
     remove(id) {
-        const that = this;
-        return new Promise(resolve => {
-            delete this.collection[id];
-            resolve();
-        });
+        return remove(this.prefix + "/" + id);
     }
 
     ids() {
-        const that = this;
-        return new Promise(resolve => {
-            resolve(Object.keys(this.collection));
-        });
+        return scan(undefined, undefined, this.prefix + "/");
     }
 
     size() {
@@ -43,11 +141,7 @@ class S3DB {
     }
 
     list() {
-        const that = this;
-        return new Promise(resolve => resolve(
-            Object
-                .keys(this.collection)
-                .map(key => this.collection[key])));
+        return fetch(undefined, undefined, this.prefix + "/");
     }
 
     sort(sorter) {
